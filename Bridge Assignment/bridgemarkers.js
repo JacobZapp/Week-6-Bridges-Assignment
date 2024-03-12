6648d898dfd9385ff844165ccb3c506444889a8e
FIRST_NAME LAST_NAME <MY_NAME@example.com>
2024-03-12 11:18:40 -0500

    added some stuff

--- Bridge Assignment/bridgemarkers.js
+++ Bridge Assignment/bridgemarkers.js
@@ -24,31 +24,31 @@ bridgeData = [
         name:'Golden Gate Bridge',
         cityState: "San Francisco and Marin, CA",
         span: 1280.2,
-        coordinates: [40.6066, -74.0447]
+        coordinates: [37.8199, -122.4783]
     },
     {
         name:'Mackinac Bridge',
         cityState: "Mackinaw and St Ignace, MI",
         span: 1298.4,
-        coordinates: [40.6066, -74.0447]
+        coordinates:  [45.8174, -84.7278]
     },
     {
         name:'George Washington Bridge',
         cityState: "New York, NY and New Jersey, NJ",
         span: 1298.4,
-        coordinates: [40.6066, -74.0447]
+        coordinates: [40.8517, -73.9527]
     },
     {
         name:'Tacoma Narrows Bridge',
         cityState: 'Tacoma and Kitsap,, WA',
         span: 1298.4,
-        coordinates: [40.6066, -74.0447]
+        coordinates: [47.2690, -122.5517]
     },
 ]
 
 // This is the marker that is at each bridge site.
 bridgeData.forEach(function(bridge) {
-    let markerText = `${bridgeData.name}<br>${bridgeData.span}`
+    let markerText = `${bridge.name}<br>${bridge.span}`
     let bridgeMarker = L.marker(bridge.coordinates)
         .bindPopup(markerText)
         .addTo(bridgeMap)
